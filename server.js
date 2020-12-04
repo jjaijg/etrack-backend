ae930a0cfdcff269c1fc4a113f399e0fd3e24482
jjaijg <jjaiganeshjg95@gmail.com>
2020-12-04 23:48:06 +0530

    Etracker - V2, port fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 var cors = require('cors');
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 const scheulers = require('./schedulers');
 
 const userRouter = require('./api/users/user.router');
